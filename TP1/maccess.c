1ecd59096852ff0fdc6fdd0959b920ff533e606d
ig1na <green_ig1na@hotmail.fr>
2016-09-20 16:49:05 +0200

    C

--- TP1/maccess.c
+++ TP1/maccess.c
@@ -1,30 +1,58 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+
 int
 main(int argc, char * argv[]){
 
   int result;
   int opt_access = 0;
+  int v_ok = 0;
   int i;
 
   for(i=2; i<argc; i++){
-      if(strcmp(argv[i],"-r")){
+      if(!strcmp(argv[i],"-r")){
 	opt_access = opt_access | R_OK;
       }
-      if(strcmp(argv[i],"-w")){
+      if(!strcmp(argv[i],"-w")){
 	opt_access = opt_access | W_OK;
       } 
-      if(strcmp(argv[i],"-x")){
+      if(!strcmp(argv[i],"-x")){
 	opt_access = opt_access | X_OK; 
       }
+      if(!strcmp(argv[i], "-v")){
+	v_ok = 1;
+      }
   }
-
+  
   result = access(argv[1], opt_access);
   
-  printf("chemin: %s\narg1: %s\narg2: %s\narg3: %s\n",argv[1], argv[2], argv[3], argv[4]);
-  printf("%d\n%d\n%d\n", result, opt_access, argc);
+  if(v_ok){
+    switch(errno){
+    case EACCES :
+      printf("Impossible d'accéder au fichier\n");
+      break;
+
+    case ELOOP :
+      printf("Trop de liens symboliques dans le chemin spécifié\n");
+      break;
+
+    case ENAMETOOLONG :
+      printf("Le chemin spécifié est trop long\n");
+      break;
+
+    case ENOENT :
+      printf("Un composant du chemin spécifié n'existe pas ou ne pointe nulle part\n");
+      break;
+
+    case ENOTDIR :
+      printf("Un composant du chemin spécifié n'est pas un répertoire\n");
+      break;
+
+    }
+
+  }
   
-  
-  return 0;
+  return result;
 }
